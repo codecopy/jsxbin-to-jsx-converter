0c792a88873c94d03ac3bd0c18a8d4cf86d8b462
autoboosh <autoboosh@gmail.com>
2015-11-10 22:01:45 +0100

    simplified loading statements

--- jsxbin_to_jsx/JsxbinDecoding/BlockStatement.cs
+++ jsxbin_to_jsx/JsxbinDecoding/BlockStatement.cs
@@ -9,7 +9,6 @@ namespace jsxbin_to_jsx.JsxbinDecoding
         LineInfo lineInfo;
         int length;
         List<INode> functionDeclarations;
-        List<INode> variableDeclarations;
         List<INode> statements;
 
         public override string Marker
@@ -33,33 +32,23 @@ namespace jsxbin_to_jsx.JsxbinDecoding
             lineInfo = DecodeBody();
             length = DecodeLength();
             functionDeclarations = new List<INode>();
-            variableDeclarations = new List<INode>();
             statements = new List<INode>();
-            if (length > 0)
+            int i = length;
+            while (i > 0)
             {
-                int i = length;
-                while (i > 0)
-                {
-                    functionDeclarations.Add(DecodeNode());
-                    i--;
-                }
-                variableDeclarations.AddRange(DecodeChildren());
+                functionDeclarations.Add(DecodeNode());
+                i--;
             }
-            else
-            {
-                statements.AddRange(DecodeChildren());
-            }
+            statements.AddRange(DecodeChildren());
         }
 
         public override string PrettyPrint()
         {
             string labels = lineInfo.CreateLabelStmt();
             var functionDeclarationsPretty = functionDeclarations.Select(f => f.PrettyPrint()).ToList();
-            var variableDeclarationsPretty = variableDeclarations.Select(g => g.PrettyPrint()).ToList();
             var statementsPretty = statements.Select(f => f.PrettyPrint()).ToList();
             string block = string.Join(
                 Environment.NewLine, functionDeclarationsPretty
-                .Concat(variableDeclarationsPretty)
                 .Concat(statementsPretty));
             return labels + block;
         }
